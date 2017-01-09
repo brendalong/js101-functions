518678277d8a516e61953c7bba8f8a2c1c199f5c
Brenda Long <brenda@studio6thave.com>
2017-01-08 23:01:56 -0600

    initial files

---
+++ README.md
@@ -0,0 +1,10 @@
+# JS102 Functions
+
+### Functions
+The basis of DRY: allows you to not repeat yourself
+
+* What is a function and characteristics?
+    * block of code designed to perform a particular task
+    * usually includes a return statement
+    * __define__ and then
+    * __invoke__
---
+++ javascripts/main.js
@@ -0,0 +1,59 @@
+//define and then invoke
+
+var returnNothing = function() {
+	console.log("I don't return anything");
+};
+returnNothing();
+
+//two ways to define
+var myFunction = function() { //function expression
+
+    return blah;
+
+};
+
+function myFunction() { //function declaration
+
+    return blah;
+
+};
+
+
+
+
+function addStuff() {
+	var sum = 2+2;
+	return sum; //return is the last thing that happens
+}
+
+var result = addStuff();
+console.log("result", result);
+
+
+// Pass data into the function by defining arguments
+function add (one,two) {
+	return one + two;
+}
+
+var addResult = add("hello", " brave new world");
+console.log("Add with args", addResult );
+
+
+
+function subtract (one, two) {
+	return one - two;
+}
+
+
+
+// Look Out - functions can take other functions as an argument. Whaaaat?
+// A function passed to another function is a lambda
+function performAction (numberOne, numberTwo, action) {
+  var meDone = action(numberOne, numberTwo);
+  return meDone;
+}
+var addValues = performAction(1, 2, add);
+var subtractValues = performAction(1, 2, subtract);
+
+console.log("addValues", addValues);
+console.log("subtractValues", subtractValues);
